7905cb2c40055247023a7e203121974719e00708
amalieri <rbshayla@protonmail.com>
2019-04-14 09:30:26 +0200

    first release!

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace nhentai_dl_gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.hentaiIdBox = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.hentaiIdBox);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 191);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hentai Info";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 16);
+            this.textBox1.TabIndex = 0;
+            // 
+            // hentaiIdBox
+            // 
+            this.hentaiIdBox.AutoSize = true;
+            this.hentaiIdBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hentaiIdBox.Location = new System.Drawing.Point(156, 20);
+            this.hentaiIdBox.Name = "hentaiIdBox";
+            this.hentaiIdBox.Size = new System.Drawing.Size(63, 16);
+            this.hentaiIdBox.TabIndex = 1;
+            this.hentaiIdBox.Text = "Hentai ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pages available: 0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Get Pages";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Download Hentai";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 224);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "nhentai downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label hentaiIdBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+namespace nhentai_dl_gui
+{
+    public partial class Form1 : Form
+    {
+        HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string getPages(string hentaiId)
+        {
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://nhentai.net/g/" + hentaiId + "/1");
+            var tagToLookFor = doc.DocumentNode.SelectSingleNode("//span[@class='num-pages']");
+            return tagToLookFor.InnerText;
+        }
+        public void downloadHentai(string hentaiID)
+        {
+            string mediaID = getMediaId(hentaiID);
+            int pages = Int32.Parse(getPages(hentaiID));
+            string urlToDownloadFrom = "https://i.nhentai.net" + "/galleries/" + mediaID + "/";
+            string pathToSave = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "\\" + hentaiID + "\\";
+            if (!Directory.Exists(pathToSave))
+            {
+                Directory.CreateDirectory(pathToSave);
+            }
+            MessageBox.Show("The program might stop responding while it is downloading, don't worry!");
+            for (int i = 1; i < pages + 1; i++)
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(urlToDownloadFrom + i.ToString() + ".jpg", @"" + pathToSave + i.ToString() + ".jpg");
+                }
+            }
+            MessageBox.Show("Your hentai has been saved to: " + pathToSave);
+
+            
+
+            
+        }
+
+        public string getMediaId(string hentaiId)
+        {
+            string mediaID = "";
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://nhentai.net/g/" + hentaiId + "/1");
+            HtmlNodeCollection imgs = doc.DocumentNode.SelectNodes("//img[@src]");
+            foreach (HtmlNode img in imgs)
+            {
+                
+                HtmlAttribute src = img.Attributes["src"];
+                var urlToTest = src.Value;
+                if (urlToTest.StartsWith("https://i.nhentai.net"))
+                {
+                    mediaID = urlToTest;
+                    break;
+                }
+            }
+            string[] parts = mediaID.Split('/');
+            string realMediaID = parts[4];
+            return realMediaID;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var test = textBox1.Text;
+            if (test == "" || test == null) {
+                MessageBox.Show("The text box can't be empty.");
+            }
+            else
+            {
+                var res = getPages(test);
+                label1.Text = "Pages available: " + res;
+                var lol = getMediaId(test);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            var test = textBox1.Text;
+            if (test == "" || test == null)
+            {
+                MessageBox.Show("The text box can't be empty.");
+            }
+            else
+            {
+                downloadHentai(test);
+            }
+        }
+    }
+}
